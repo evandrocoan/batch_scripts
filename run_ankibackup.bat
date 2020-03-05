efa0a2951e0398f2178e2627338cdac0ef504376
evandrocoan <evandrocoan@hotmail.com>
2020-03-05 18:08:57 -0300

    Fixed run_ankibackup.bat not returning the exit code

--- run_ankibackup.bat
+++ run_ankibackup.bat
@@ -1,8 +1,9 @@
-
 :: Create a Task Scheduler with wscript as the program and the following as the arguments:
 :: "D:\User\Dropbox\SoftwareVersioning\SpeakTimeVBScript\silent_run.vbs" "D:\User\Dropbox\SoftwareVersioning\SpeakTimeVBScript\run_ankibackup.bat"
 
 SET "CURRENT_PATH=%~dp0"
 
-sh /cygdrive/f/cygwin/home/Professional/scripts/backupanki.sh >> "%CURRENT_PATH%\run_ankibackup.log" 2>&1
+setlocal enabledelayedexpansion
+
+sh /cygdrive/f/cygwin/home/Professional/scripts/backupanki.sh >> "%CURRENT_PATH%\run_ankibackup.log" 2>&1 || exit !errorlevel!
 
