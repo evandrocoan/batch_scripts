84506a57db2753cb9a2ed4cd43a9017a2868f9bf
evandrocoan <evandrocoan@hotmail.com>
2020-04-23 18:02:29 -0300

    Deprecated the run_ankibackup.bat in favor of
    https://github.com/evandrocoan/dotfiles/commit/4b16c521b4c3a80198790a94cd423fa4b4691891

--- run_ankibackup.bat
+++
@@ -1,9 +0,0 @@
-:: Create a Task Scheduler with wscript as the program and the following as the arguments:
-:: "D:\User\Dropbox\SoftwareVersioning\SpeakTimeVBScript\silent_run.vbs" "D:\User\Dropbox\SoftwareVersioning\SpeakTimeVBScript\run_ankibackup.bat"
-
-SET "CURRENT_PATH=%~dp0"
-
-setlocal enabledelayedexpansion
-
-sh /cygdrive/f/cygwin/home/Professional/scripts/backupanki.sh >> "%CURRENT_PATH%\run_ankibackup.log" 2>&1 || exit !errorlevel!
-
