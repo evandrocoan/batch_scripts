b6d1ed8d5ffdb7016b96a4ae3edc19590f5e413f
evandrocoan <evandrocoan@hotmail.com>
2020-08-23 01:11:31 -0300

    Updated disable_all_wakeup_devices.bat

--- disable_all_wakeup_devices.bat
+++ disable_all_wakeup_devices.bat
@@ -1,34 +1,38 @@
 
 
 @echo on
+@echo off
 
 :: List all devices
-echo .
-echo Before
+echo Before, powercfg devicequery wake_armed
+powercfg devicequery wake_armed
 
 :: https://stackoverflow.com/questions/6359820/how-to-set-commands-output-as-a-variable-in-a-batch-file
 SETLOCAL ENABLEDELAYEDEXPANSION
 SET count=1
 FOR /F "tokens=* USEBACKQ" %%F IN (`powercfg devicequery wake_armed`) DO (
-  SET usb_devices!count!=%%F
-  SET /a count=!count!+1
+    SET usb_devices!count!=%%F
+    SET /a count=!count!+1
 )
-ENDLOCAL & (
-	set usb_devices1=%usb_devices1%
-)
+ENDLOCAL
 
 :: https://stackoverflow.com/questions/39276105/windows-batch-if-statement-with-user-input
 ECHO %usb_devices1%
-
-IF "%usb_devices1%"=="NONE" GOTO :end
+:: IF "%usb_devices1%"=="NONE" GOTO :end
 
 :: powercfg devicedisablewake "HID-compliant mouse (005)"
 :: https://superuser.com/questions/631637/how-to-disallow-usb-devices-to-wake-the-computer-by-default-in-windows-7
-for /F "tokens=*" %%A in ('powercfg -devicequery wake_armed') do echo Disabling device: "%%A" && powercfg -devicedisablewake "%%A"
-
-echo .
-echo After
-powercfg devicequery wake_armed
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET count=1
+for /F "tokens=*" %%A in ('powercfg -devicequery wake_from_any') do (
+    echo.
+    echo Disabling device !count!: "%%A" && powercfg -devicedisablewake "%%A"
+    SET /a count=!count!+1
+)
+ENDLOCAL
 
 :end
-:: pause
+echo After, powercfg devicequery wake_armed
+powercfg devicequery wake_armed
+pause
+
