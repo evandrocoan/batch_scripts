26d9e499d32204e9406c807225f9f121c64a336b
evandrocoan <evandrocoan@hotmail.com>
2020-11-15 11:40:22 -0300

    Fixed Anki being called with the out argument

--- run_anki.bat
+++ run_anki.bat
@@ -1,5 +1,14 @@
 
-sh run_anki.sh %* || goto :fail
+:: https://stackoverflow.com/questions/935609/batch-parameters-everything-after-1
+if not "%1"=="out" goto run_anki_now
+echo all arguments: %*
+set _all=%*
+call set _tail=%%_all:*%2=%%
+set _tail=%2%_tail%
+echo tail arguments: %_tail%
+
+:run_anki_now
+sh run_anki.sh %_tail% || goto :fail
 
 :: Exit the batch file, without closing the cmd.exe, if called from another script
 if not "%1"=="" exit 0
