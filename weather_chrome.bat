91263640f0310b99b605f0296ba04d2f87d144ad
evandrocoan <evandrocoan@hotmail.com>
2023-07-23 16:13:34 -0300

    Created weather_chrome.bat

---
+++ weather_chrome.bat
@@ -0,0 +1,5 @@
+"C:\Program Files\Google\Chrome\Application\chrome.exe" ^
+	--new-window ^
+	--user-data-dir="F:\GoogleChromeProfiles\MainProfile" ^
+	"https://weather.com/weather/hourbyhour/l/af83edc23cc660c4cf43691a22012da6c5732052edb4ea4de3fe3714a5e2bd98" ^
+	"https://www.accuweather.com/en/br/florianópolis/35952/hourly-weather-forecast/35952"
