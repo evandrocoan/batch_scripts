d9194baccacac3d7071645aaaa7218548f783eef
evandrocoan <evandrocoan@hotmail.com>
2026-01-04 21:18:16 -0300

    Fixed remove-crunchyroll-simulcastcalendar-dub-videos.js to work with
    new title style

--- tampermonkey/remove-crunchyroll-simulcastcalendar-dub-videos.js
+++ tampermonkey/remove-crunchyroll-simulcastcalendar-dub-videos.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove crunchyroll simulcastcalendar dub videos
 // @namespace    *
-// @version      0.1
+// @version      0.3
 // @description  https://webapps.stackexchange.com/questions/145612/how-to-stop-the
 // @author       You
 //
@@ -11,16 +11,47 @@
 
 (function() {
     'use strict';
+
+    // =======================================================================
+    // CONFIGURATION: Languages to HIDE (dubbed versions you don't want to see)
+    // Comment out or remove any language you want to KEEP
+    // =======================================================================
+    const languagesToHide = [
+        'English',
+        'Deutsch',
+        'Français',
+        'Español (América Latina)',
+        'Español (España)',
+        'Português (Brasil)',
+        'Italiano',
+        'Русский',
+        'العربية',
+        'हिंदी',
+        'தமிழ்',
+        'తెలుగు',
+        'ไทย',
+        'Bahasa Indonesia',
+        // '中文 (普通话)',  // Chinese - keep this for donghua (Chinese anime)
+    ];
+
+    // Build a regex pattern to match titles ending with (Language)
+    const langPattern = languagesToHide.map(lang => lang.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|');
+    const dubbedRegex = new RegExp(`\\((${langPattern})\\)\\s*$`);
+
     let keepclosing = async () => {
-        let viewedbar = $('cite:contains(" Dub)")');
-        if(viewedbar.length) {
-            // console.log("removing running...");
-            let parent = viewedbar.closest('li');
-            if(parent.length) {
-                // use hide instead of remove to avoid mass flickering of the page
-                parent.hide();
+        // Find all season name cite elements
+        $('h1.season-name cite[itemprop="name"]').each(function() {
+            let title = $(this).text().trim();
+
+            // Check for old format " Dub)" or new format "(Language)"
+            if (title.includes(' Dub)') || dubbedRegex.test(title)) {
+                let parent = $(this).closest('li');
+                if (parent.length) {
+                    // use hide instead of remove to avoid mass flickering of the page
+                    parent.hide();
+                }
             }
-        }
+        });
         // console.log("running");
         // setTimeout(keepclosing, 500);
     }
